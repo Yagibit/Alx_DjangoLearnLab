a1c774cff86d8e72c9e7e136f835e6c4ef6ebb47
YagiDao12 <yagidaobit@gmail.com>
2026-02-08 02:13:05 +0300

    added

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,3 +5,11 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
